24bbb7716886c261a189e321d504968b6b5cf741
Alexis Hope <lex@Alexiss-MacBook-Pro.local>
2017-01-22 16:21:50 +1100

    add basic tab nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ $(function () {
     var searchForm = $('form[name=flightSearch]'),
         strToTime  = d3.timeParse('%d/%m/%Y');
     
+    var dateNav = $('#search-results li');
+    
     
         
     searchForm.submit(function (event) {
@@ -12,5 +14,11 @@ $(function () {
         console.log(date, d3.timeDay.offset(strToTime(date), -24));
         
         event.preventDefault();
-    })
+    });
+    
+    dateNav.click(function (event) {
+        var index = dateNav.index(event.target);
+        $('#search-results li.active').removeClass('active');
+        dateNav.eq(index).addClass('active');
+    });
 })
